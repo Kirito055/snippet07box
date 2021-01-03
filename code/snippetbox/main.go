75271ae972255b5405bf067839bf8405b3a5351d
Talgat Baimaganbetov <talgat.baymaganbetov@mail.ru>
2021-01-03 23:54:57 +0600

    Https status code

--- code/snippetbox/main.go
+++ code/snippetbox/main.go
@@ -6,21 +6,29 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request)  {
-	w.Write([]byte("Hello from snippetbossssx"))
+
+	w.Write([]byte("Hello from snippetbox"))
 
 }
 func showSnippet(w http.ResponseWriter,r *http.Request)  {
 	w.Write([]byte("Display a specific snippet..."))
 }
 func createSnippet(w http.ResponseWriter,r *http.Request)  {
+	if r.Method!=http.MethodPost{
+		w.Header().Set("Allow",http.MethodPost)
+		w.WriteHeader(405)
+		w.Write([]byte("Method Not Allowed"))
+		return
+	}
 	w.Write([]byte("Create new snippet"))
 }
 func main()  {
 	mux:=http.NewServeMux()
+
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet",showSnippet)
 	mux.HandleFunc("/snippet/create",createSnippet)
 	log.Println("Starting server on :4001")
-	err:=http.ListenAndServe(":4001",mux)
+	err:=http.ListenAndServe(":4001",nil)
 	log.Fatal(err)
 	}
