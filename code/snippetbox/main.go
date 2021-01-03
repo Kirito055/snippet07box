a6e9db2c0114e4dc175342311522374964a152bb
Talgat Baimaganbetov <talgat.baymaganbetov@mail.ru>
2021-01-03 23:30:24 +0600

    Next level Snippet

--- code/snippetbox/main.go
+++ code/snippetbox/main.go
@@ -1,7 +1,26 @@
 package  main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
-func  sayHello()  {
-	fmt.Println("Hello world!")
+func home(w http.ResponseWriter, r *http.Request)  {
+	w.Write([]byte("Hello from snippetbossssx"))
+
 }
+func showSnippet(w http.ResponseWriter,r *http.Request)  {
+	w.Write([]byte("Display a specific snippet..."))
+}
+func createSnippet(w http.ResponseWriter,r *http.Request)  {
+	w.Write([]byte("Create new snippet"))
+}
+func main()  {
+	mux:=http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet",showSnippet)
+	mux.HandleFunc("/snippet/create",createSnippet)
+	log.Println("Starting server on :4001")
+	err:=http.ListenAndServe(":4001",mux)
+	log.Fatal(err)
+	}
