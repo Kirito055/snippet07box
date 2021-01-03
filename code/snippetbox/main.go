1e70abc38fa2a27e0085c17e137712c99f7a553d
Talgat Baimaganbetov <talgat.baymaganbetov@mail.ru>
2021-01-04 00:33:23 +0600

    Chapter 2.7

--- code/snippetbox/main.go
+++ code/snippetbox/main.go
@@ -1,8 +1,10 @@
 package  main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request)  {
@@ -10,14 +12,24 @@ func home(w http.ResponseWriter, r *http.Request)  {
 	w.Write([]byte("Hello from snippetbox"))
 
 }
-func showSnippet(w http.ResponseWriter,r *http.Request)  {
-	w.Write([]byte("Display a specific snippet..."))
+func showSnippet(w http.ResponseWriter,r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+	http.NotFound(w,r)
+		return
+	}
+
+fmt.Fprint(w,"Display a scecific snippet with ID %d...",id)
 }
 func createSnippet(w http.ResponseWriter,r *http.Request)  {
 	if r.Method!=http.MethodPost{
+		w.Header().Add("Create-control","public")
+		w.Header().Add("Create-control"," max-age=645454")
 		w.Header().Set("Allow",http.MethodPost)
-		w.WriteHeader(405)
-		w.Write([]byte("Method Not Allowed"))
+	http.Error(w,"Method Not Aloowd",405)
+
+		w.Header().Get("Create-control")
+		w.Header().Del("Create-control")
 		return
 	}
 	w.Write([]byte("Create new snippet"))
@@ -29,6 +41,6 @@ func main()  {
 	mux.HandleFunc("/snippet",showSnippet)
 	mux.HandleFunc("/snippet/create",createSnippet)
 	log.Println("Starting server on :4001")
-	err:=http.ListenAndServe(":4001",nil)
+	err:=http.ListenAndServe(":4001",mux)
 	log.Fatal(err)
 	}
